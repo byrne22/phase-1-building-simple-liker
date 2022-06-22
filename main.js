02add05f0f82cf708a0f8293b1a3f68dc27d9778
byrne22 <byron.odhiambo@student.moringaschool.com>
2022-06-21 20:22:37 -0400

    Done.

--- main.js
+++ main.js
@@ -4,6 +4,42 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const modal = document.querySelector("#modal");
+modal.classList.add("hidden");
+
+
+const click = (event) =>{
+  const fig = event.target
+  // console.log(event.target.innerText)
+  fig.innerText = EMPTY_HEART
+  mimicServerCall()
+    .then( res => {
+      if(fig.innerText === EMPTY_HEART){
+        fig.innerText = FULL_HEART
+        fig.classList.add("activated-heart")
+      }
+      else{
+        fig.innerText = EMPTY_HEART
+        fig.classList.remove("activated-heart")
+      }
+      
+      
+    })
+    .catch(err => {
+      modal.classList.remove("hidden");
+      modal.querySelector("#modal-message").textContent = err;
+      setTimeout( () => {
+        modal.classList.add("hidden")
+      }, 3000)
+    }) 
+   
+}
+
+
+const hearts = document.querySelectorAll(".like-glyph");
+hearts.forEach( heart => {
+  heart.addEventListener("click", click)
+})
 
 
 
